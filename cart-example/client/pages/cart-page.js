63bd36029b71db7a22c2f79c7dfee45196e2aa5e
yepsson <32198001+yepsson@users.noreply.github.com>
2018-05-25 10:05:07 +0200

    cart start

--- cart-example/client/pages/cart-page.js
+++ cart-example/client/pages/cart-page.js
@@ -2,23 +2,19 @@ const CartPageComponent = {
 
     template: `
       <div class="row">
-        <product
-          v-for="product in products"
-          v-bind:item="product"
-          v-bind:key="product._id"
-        ></product>
+        <cart class="col-12"></cart>
       </div>
     `,
-    created(){
-      http.get('/rest/products').then((response)=>{
-        this.products = response.data;
-      }).catch((error) =>{
-        console.error(error);
-      });
-    } ,
-    data(){
-      return{
-        products: []
-      }
-    }
+    // created(){
+    //   http.get('/rest/products').then((response)=>{
+    //     this.products = response.data;
+    //   }).catch((error) =>{
+    //     console.error(error);
+    //   });
+    // } ,
+    // data(){
+    //   return{
+    //     products: []
+    //   }
+    // }
   }
